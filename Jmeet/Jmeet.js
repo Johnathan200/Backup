248fb7d82163ec756970dbd121d38d458eaea6ef
Johnthan <johnathan2n123@gmail.com>
2022-02-23 14:01:15 +0530

    trying login js part

--- Jmeet/Jmeet.js
+++ Jmeet/Jmeet.js
@@ -1,3 +1,6 @@
-function formjs() {
-    document.getElementById("form").style.visibility = "visible";
+alert("Worked");
+function one() {
+    var checker = true;
+    var div1 = document.getElementById('form');
+    div1.style.display("block");
 }
